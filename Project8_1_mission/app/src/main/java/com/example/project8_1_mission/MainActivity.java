d447f3370aa9b18228aae6169990f9d2d28fbf9a
강명석 <k4858678@gmail.com>
2023-04-06 20:24:14 +0900

    오픈소스 전문프로젝트 5주차 과제

---
+++ Project8_1_mission/app/src/main/java/com/example/project8_1_mission/MainActivity.java
@@ -0,0 +1,101 @@
+package com.example.project8_1_mission;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Calendar;
+
+public class MainActivity extends AppCompatActivity {
+
+    DatePicker dp;
+    EditText edtDiary;
+    Button btnWrite;
+    String fileName;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setTitle("간단 일기장");
+
+        dp = findViewById(R.id.datePicker1);
+        edtDiary = findViewById(R.id.edtDiary);
+        btnWrite = findViewById(R.id.btnWrite);
+
+        Calendar cal = Calendar.getInstance();
+        int cYear = cal.get(Calendar.YEAR);
+        int cMonth = cal.get(Calendar.MONTH);
+        int cDay = cal.get(Calendar.DAY_OF_MONTH);
+
+        dp.init(cYear, cMonth, cDay, new DatePicker.OnDateChangedListener() {
+            @Override
+            public void onDateChanged(DatePicker datePicker, int year, int monthOfYear, int dayOfMonth) {
+                fileName = Integer.toString(year) + "_" + Integer.toString(monthOfYear + 1) + "_"
+                        + Integer.toString(dayOfMonth) + ".txt";
+                String str = readDiary(fileName);
+                edtDiary.setText(str);
+                btnWrite.setEnabled(true);
+            }
+        });
+
+        btnWrite.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    FileOutputStream outFs = openFileOutput(fileName, Context.MODE_PRIVATE);
+                    String str = edtDiary.getText().toString();
+                    outFs.write(str.getBytes());
+                    outFs.close();
+                    Toast.makeText(getApplicationContext(), fileName+"이 저장됨", Toast.LENGTH_LONG).show();
+                } catch (IOException e) {
+
+                }
+            }
+        });
+
+        /* 처음 실행시 일기 있는지 확인 */
+        FileInputStream inFs;
+        String fileName = Integer.toString(Calendar.getInstance().get(Calendar.YEAR)) + "_"
+                + Integer.toString(Calendar.getInstance().get(Calendar.MONTH) + 1) + "_"
+                + Integer.toString(Calendar.getInstance().get(Calendar.DAY_OF_MONTH)) + ".txt";
+
+        try {
+            inFs = openFileInput(fileName);
+            byte[] txt = new byte[500];
+            inFs.read(txt);
+            inFs.close();
+            edtDiary.setText((new String(txt)).trim());
+            btnWrite.setText("수정하기");
+        } catch (IOException e) {
+            edtDiary.setHint("일기 없음");
+            btnWrite.setText("새로 저장");
+        }
+    }
+
+    String readDiary(String fName) {
+        String diaryStr = null;
+        FileInputStream inFs;
+        try {
+            inFs = openFileInput(fName);
+            byte[] txt = new byte[500];
+            inFs.read(txt);
+            inFs.close();
+            diaryStr = (new String(txt)).trim();
+            btnWrite.setText("수정하기");
+        } catch (IOException e) {
+            edtDiary.setHint("일기 없음");
+            btnWrite.setText("새로 저장");
+        }
+        return diaryStr;
+    }
+}
